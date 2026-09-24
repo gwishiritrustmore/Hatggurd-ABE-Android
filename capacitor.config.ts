ad5bfd56c72d951284dccca020d35b0af1f70c97
Trustmore Tawanda Gwishiri <gwishiritrustmore@gmail.com>
2026-09-24 10:14:06 +0200

    Add Capacitor configuration

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'zw.hatggurd.abe',
+  appName: 'Hatggurd ABE',
+  webDir: 'www'
+};
+
+export default config;
